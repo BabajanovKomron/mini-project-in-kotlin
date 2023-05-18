7ad75255870604641d093492354b10263ad37c30
LordKomron <KomronBabajanov0747@gmail.com>
2023-05-18 15:15:35 +0500

    Update

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/myfirstproject/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.myfirstproject.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.myfirstproject.test";
+  public static final String BUILD_TYPE = "debug";
+}
